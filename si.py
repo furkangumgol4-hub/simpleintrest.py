d2945ccc7338cac52fd3d63cff13e6496ad8494a
furkangumgol4 <furkangumgol4@gmail.com>
2025-10-23 14:45:01 +0530

    save

--- si.py
+++ si.py
@@ -4,3 +4,13 @@ total_salary = salary + bonus
 
 print("Original Salary:", salary)
 print("Bonus Added:", total_salary)
+
+salary = float(input("Enter your salary: "))
+bonus = salary * 0.10
+total = salary + bonus
+tax = total * 0.05
+final_salary = total - tax
+
+print("Original Salary:", salary)
+print("Bonus Added:", total)
+print("After Tax Deduction:", final_salary)
